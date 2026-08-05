07d321b432df35dbff1c94a14318b3933cb616bd
LanceHuang245 <LanceHuang245@outlook.com>
2026-08-05 14:57:20 +0800

    chore: bump version

--- lib/app_constants.dart
+++ lib/app_constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String appName = 'Zephyr';
-  static const String appVersion = 'v2.2.5';
+  static const String appVersion = 'v2.3.0';
   static const String appDescription =
       'Simple and fast real-time weather forecast software.';
   static const String appUrl = 'https://github.com/LanceHuang245/Zephyr';
